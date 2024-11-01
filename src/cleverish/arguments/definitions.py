9ba9e6d06274324da70e4513ee8d40e5a571543a
Soumyo Deep Gupta <d33p0st@gmail.com>
2024-11-01 22:17:47 +0530

    Add all ingredients for v0.0.1 (#1)
    * Add: source files for v0.0.1. All basic functionalities provided and tested.
    
    * Add: initial tests for initialization has been added, manual testing done on other features for now
    
    * Add: a python based .gitignore file with extra entries -> .vscode and .DS_Store (for macos)
    
    * Add: a demo clever.toml for reference
    
    * Add: Code of Conduct
    
    * Add: Contributing instructions
    
    * Add: Manifest for including stubs in dist.
    
    * Add: pypi configs in pyproject.toml
    
    * Add: a stable README
    
    * Add: requirements.txt for incoming contributions (see CONTRIBUTING.md)
    
    * Update README: added a line to redirect users to clever.toml file for reference

---
+++ src/cleverish/arguments/definitions.py
@@ -0,0 +1,26 @@
+class ArgumentDefinition:
+    __arguments__ = {
+        # Add Command
+        "add": "-a",
+
+        # shell command
+        "shell": '-sh',
+
+        # Init command
+        "init": "-i",
+        "force": "-f",
+        "name": "-n",
+        "package": "-pkg",
+
+        # Install commad
+        "install": "-in",
+
+        # Build Command,
+        "build": "-b",
+
+        # upload command,
+        "upload": "-u",
+
+        # Help Command
+        "help": "-h",
+    }
